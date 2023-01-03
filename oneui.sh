66df6216de43bfbd6eb85cf48bc89c1c80238f94
PoomSmart <thatchapon.unp@outlook.com>
2023-01-03 21:24:16 +0700

    Oopsie

--- oneui.sh
+++ oneui.sh
@@ -7,22 +7,22 @@ NAME=oneui
 FONT_NAME=NotoColorEmoji
 FONT_PATH=$NAME/$FONT_NAME.ttf
 
-# rm -rf $NAME/images
-# mkdir -p $NAME/images/96 $NAME/images/64 $NAME/images/48 $NAME/images/40 $NAME/images/32 $NAME/images/20
+rm -rf $NAME/images
+mkdir -p $NAME/images/96 $NAME/images/64 $NAME/images/48 $NAME/images/40 $NAME/images/32 $NAME/images/20
 
-# echo "Extracting font..."
-# cd $NAME
-# ttx -q -s -z extfile $FONT_NAME.ttf
-# cd ..
+echo "Extracting font..."
+cd $NAME
+ttx -q -s -z extfile $FONT_NAME.ttf
+cd ..
 
-# echo "Copying and resizing PNGs..."
-# mogrify -resize 96x96 -path $NAME/images/96 $NAME/bitmaps/strike0/*.png
-# mogrify -resize 64x64 -path $NAME/images/64 $NAME/images/96/*.png
-# mogrify -resize 48x48 -path $NAME/images/48 $NAME/images/64/*.png
-# mogrify -resize 40x40 -path $NAME/images/40 $NAME/images/48/*.png
-# mogrify -resize 32x32 -path $NAME/images/32 $NAME/images/40/*.png
-# mogrify -resize 20x20 -path $NAME/images/20 $NAME/images/32/*.png
-# rm -rf $NAME/bitmaps
+echo "Copying and resizing PNGs..."
+mogrify -resize 96x96 -path $NAME/images/96 $NAME/bitmaps/strike0/*.png
+mogrify -resize 64x64 -path $NAME/images/64 $NAME/images/96/*.png
+mogrify -resize 48x48 -path $NAME/images/48 $NAME/images/64/*.png
+mogrify -resize 40x40 -path $NAME/images/40 $NAME/images/48/*.png
+mogrify -resize 32x32 -path $NAME/images/32 $NAME/images/40/*.png
+mogrify -resize 20x20 -path $NAME/images/20 $NAME/images/32/*.png
+rm -rf $NAME/bitmaps
 
 python3 $NAME.py common/${APPLE_FONT_NAME}_00.ttf $NAME/$FONT_NAME.ttf $NAME/$FONT_NAME.G_S_U_B_.ttx
 python3 $NAME.py common/${APPLE_FONT_NAME}_01.ttf $NAME/$FONT_NAME.ttf $NAME/$FONT_NAME.G_S_U_B_.ttx
