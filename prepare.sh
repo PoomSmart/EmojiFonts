f73f690690e21ac8191e3d655ad19c04724cc3de
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2025-11-29 16:06:23 +0700

    Fix uv things

--- prepare.sh
+++ prepare.sh
@@ -10,11 +10,11 @@ mkdir -p $WORK_DIR
 rm -f *.ttx
 
 echo "Extracting font..."
-otc2otf $IOS_FONT_NAME.ttc
+uv run otc2otf $IOS_FONT_NAME.ttc
 mv AppleColorEmoji.ttf $WORK_DIR/${IOS_FONT_NAME}_00.ttf
 mv .AppleColorEmojiUI.ttf $WORK_DIR/${IOS_FONT_NAME}_01.ttf
 
-otc2otf $MAC_FONT_NAME.ttc
+uv run otc2otf $MAC_FONT_NAME.ttc
 mv AppleColorEmoji.ttf $WORK_DIR/${MAC_FONT_NAME}_00.ttf
 rm .AppleColorEmojiUI.ttf
 
@@ -23,13 +23,13 @@ echo "Extracting tables..."
 # Un-shared tables: ['head', 'hhea', 'meta', 'name', 'trak', 'cntr']
 UNSHARED_TABLES='-t head -t hhea -t meta -t name -t trak -t cntr'
 SHARED_TABLES='-x GDEF -x DSIG -x cmap -x feat -x glyf -x hmtx -x loca -x maxp -x morx -x post -x sbix -x vhea -x vmtx -x GPOS -x GlyphOrder -x OS/2'
-ttx -q -s -f -x DSIG ${IOS_FONT_NAME}_00.ttf
-ttx -q -s -f $SHARED_TABLES ${IOS_FONT_NAME}_01.ttf
-ttx -q -s -f -t sbix ${MAC_FONT_NAME}_00.ttf
+uv run ttx -q -s -f -x DSIG ${IOS_FONT_NAME}_00.ttf
+uv run ttx -q -s -f $SHARED_TABLES ${IOS_FONT_NAME}_01.ttf
+uv run ttx -q -s -f -t sbix ${MAC_FONT_NAME}_00.ttf
 
 echo "Fixing up interracial emojis..."
-uv run ../shift-multi.py ${IOS_FONT_NAME}_00._h_m_t_x.ttx
-uv run ../remove-class3.py ${IOS_FONT_NAME}_00.G_D_E_F_.ttx
+uv run shift-multi ${IOS_FONT_NAME}_00._h_m_t_x.ttx
+uv run remove-class3 ${IOS_FONT_NAME}_00.G_D_E_F_.ttx
 
 for ttx in $(find . -type f -name "${IOS_FONT_NAME}_00.*.ttx")
 do
@@ -39,7 +39,7 @@ done
 sed 's/_00/_01/g' ${IOS_FONT_NAME}_00.ttx > ${IOS_FONT_NAME}_01.ttx
 
 echo "Building fonts..."
-ttx -q -o ${IOS_FONT_NAME}_00.ttf -b ${IOS_FONT_NAME}_00.ttx
-ttx -q -o ${IOS_FONT_NAME}_01.ttf -b ${IOS_FONT_NAME}_01.ttx
+uv run ttx -q -o ${IOS_FONT_NAME}_00.ttf -b ${IOS_FONT_NAME}_00.ttx
+uv run ttx -q -o ${IOS_FONT_NAME}_01.ttf -b ${IOS_FONT_NAME}_01.ttx
 
 cd ..
