089340fe64377d09664122688bb836dec162bea0
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2026-03-28 14:44:34 +0700

    Parallel image resizing

--- resize.sh
+++ resize.sh
@@ -6,17 +6,24 @@ HD=$1
 [[ $2 = true ]] && COLORS=256 || COLORS=
 [[ $3 = true ]] && RESIZE_96=true || RESIZE_96=false
 
+JOBS=$(sysctl -n hw.logicalcpu)
+
 if [[ $HD = true ]]; then
-    pngquant --skip-if-larger $COLORS -f --ext .png images/160/*.png || true
-    mogrify -resize 96x96 -path images/96 images/160/*.png
-    pngquant --skip-if-larger $COLORS -f --ext .png images/96/*.png || true
+    find images/160 -maxdepth 1 -name '*.png' -print0 | xargs -0 -P "$JOBS" -n 1 pngquant --skip-if-larger $COLORS -f --ext .png || true
+    find images/160 -maxdepth 1 -name '*.png' -print0 | \
+        xargs -0 -P "$JOBS" -I {} bash -c 'magick "$1" -resize 96x96 "images/96/$(basename "$1")"' _ {}
+    find images/96 -maxdepth 1 -name '*.png' -print0 | xargs -0 -P "$JOBS" -n 1 pngquant --skip-if-larger $COLORS -f --ext .png || true
 fi
 
-[[ $RESIZE_96 = true ]] && mogrify -resize 96x96 images/96/*.png
-pngquant --skip-if-larger $COLORS -f --ext .png images/96/*.png || true
-mogrify -resize 64x64 -path images/64 images/96/*.png
-pngquant --skip-if-larger $COLORS -f --ext .png images/64/*.png || true
-mogrify -resize 40x40 -path images/40 images/64/*.png
-pngquant --skip-if-larger $COLORS -f --ext .png images/40/*.png || true
+if [[ $RESIZE_96 = true ]]; then
+    find images/96 -maxdepth 1 -name '*.png' -print0 | xargs -0 -P "$JOBS" -I {} magick {} -resize 96x96 {}
+fi
+find images/96 -maxdepth 1 -name '*.png' -print0 | xargs -0 -P "$JOBS" -n 1 pngquant --skip-if-larger $COLORS -f --ext .png || true
+find images/96 -maxdepth 1 -name '*.png' -print0 | \
+    xargs -0 -P "$JOBS" -I {} bash -c 'magick "$1" -resize 64x64 "images/64/$(basename "$1")"' _ {}
+find images/64 -maxdepth 1 -name '*.png' -print0 | xargs -0 -P "$JOBS" -n 1 pngquant --skip-if-larger $COLORS -f --ext .png || true
+find images/64 -maxdepth 1 -name '*.png' -print0 | \
+    xargs -0 -P "$JOBS" -I {} bash -c 'magick "$1" -resize 40x40 "images/40/$(basename "$1")"' _ {}
+find images/40 -maxdepth 1 -name '*.png' -print0 | xargs -0 -P "$JOBS" -n 1 pngquant --skip-if-larger $COLORS -f --ext .png || true
 
 oxipng -q images/*/*.png
